dd86bb0b08a01981d856560ebc064ad9263a9df5
THIRUMAVALAVAN M <aravindsagar06@gmail.com>
2025-01-20 09:53:55 +0530

    commit

--- UserInputs/src/main/java/com/example/UserInputs/Entities/NewConstruction.java
+++ UserInputs/src/main/java/com/example/UserInputs/Entities/NewConstruction.java
@@ -10,7 +10,7 @@ public class NewConstruction {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @OneToOne
+    @OneToOne(cascade = CascadeType.ALL)
     @JoinColumn(name = "project_id", nullable = false)
     private ProjectInputs project;
 
@@ -29,7 +29,60 @@ public class NewConstruction {
     @Column
     private String specialRequirements; // e.g., "Solar Panels", "Rainwater Harvesting"
 
-    // Getters and setters
-    // Constructors
-    
+    // Getters and Setters
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public ProjectInputs getProject() {
+        return project;
+    }
+
+    public void setProject(ProjectInputs project) {
+        this.project = project;
+    }
+
+    public int getNumberOfFloors() {
+        return numberOfFloors;
+    }
+
+    public void setNumberOfFloors(int numberOfFloors) {
+        this.numberOfFloors = numberOfFloors;
+    }
+
+    public int getNumberOfRooms() {
+        return numberOfRooms;
+    }
+
+    public void setNumberOfRooms(int numberOfRooms) {
+        this.numberOfRooms = numberOfRooms;
+    }
+
+    public boolean isIncludesBasement() {
+        return includesBasement;
+    }
+
+    public void setIncludesBasement(boolean includesBasement) {
+        this.includesBasement = includesBasement;
+    }
+
+    public boolean isIncludesGarage() {
+        return includesGarage;
+    }
+
+    public void setIncludesGarage(boolean includesGarage) {
+        this.includesGarage = includesGarage;
+    }
+
+    public String getSpecialRequirements() {
+        return specialRequirements;
+    }
+
+    public void setSpecialRequirements(String specialRequirements) {
+        this.specialRequirements = specialRequirements;
+    }
 }
