2c883fe1e11acf81d6678616dab0eca3e35bcdbc
THIRUMAVALAVAN M <aravindsagar06@gmail.com>
2025-01-21 14:48:48 +0530

    Delete UserInputs directory

--- UserInputs/src/main/java/com/example/UserInputs/Repository/NewConstructionRepo.java
+++
@@ -1,7 +0,0 @@
-package com.example.UserInputs.Repository;
-
-import com.example.UserInputs.Entities.NewConstruction;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface NewConstructionRepo extends JpaRepository<NewConstruction,Long> {
-}
--- UserInputs/src/main/java/com/example/UserInputs/Repository/ProjectInputsRepo.java
+++
@@ -1,7 +0,0 @@
-package com.example.UserInputs.Repository;
-
-import com.example.UserInputs.Entities.ProjectInputs;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ProjectInputsRepo extends JpaRepository<ProjectInputs,Long> {
-}
