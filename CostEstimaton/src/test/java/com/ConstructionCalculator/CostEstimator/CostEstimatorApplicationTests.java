1fff2bc3a738e58bc7ca416ea3f629366d402f83
THIRUMAVALAVAN M <aravindsagar06@gmail.com>
2025-01-23 16:05:12 +0530

    renamed as COSTESTIMATION

---
+++ CostEstimaton/src/test/java/com/ConstructionCalculator/CostEstimator/CostEstimatorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ConstructionCalculator.CostEstimator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CostEstimatorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
