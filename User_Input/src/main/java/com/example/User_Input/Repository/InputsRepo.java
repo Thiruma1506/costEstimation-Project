9d83686991b4532e439344afaaf21869a45ebc60
THIRUMAVALAVAN M <aravindsagar06@gmail.com>
2025-01-27 15:59:50 +0530

    Delete User_Input directory

--- User_Input/src/main/java/com/example/User_Input/Repository/InputsRepo.java
+++
@@ -1,8 +0,0 @@
-package com.example.User_Input.Repository;
-
-import com.example.User_Input.Entity.Inputs;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface InputsRepo extends JpaRepository<Inputs,Long> {
-
-}
